9db6a96cc67904c27e98e449f812050049c0e20c
devSharma14 <devs140124@gmail.com>
2024-08-25 11:35:39 +0530

    Creating user and video model

---
+++ src/models/user.model.js
@@ -0,0 +1,49 @@
+import mongoose, {Schema} from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    fullName: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true
+    },
+    avatar: {
+        type: String,
+        required: true,
+    },
+    converImage: {
+        type: String,
+    },
+    watchHistory: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Video"
+        }
+    ],
+    password: {
+        type: String,
+        required: true,
+    },
+    refreshToken: {
+        type: String
+    }
+}, {timestamps: true})
+
+export const User = mongoose.model("User", userSchema);
